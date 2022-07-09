7029dee306760846dcf44f4bd092cd8c656c148c
jesebe <jsbenitez256@misena.edu.co>
2022-07-08 23:15:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+//Creacion del servidor
+const app = express();
+
+//conectar a la BD
+conectarDB();
+
+app.use(cors())
+
+
+app.use(express.json())
+
+
+app.use('/api/productos', require('./routes/producto'));
+
+
+app.listen(4000, () => {
+    console.log('El servidor se levanto')
+})
